0e4f809dd4fc5e4b8b6332b8dd3410dbf22f4a5b
kremrik <kyle.robert.e@gmail.com>
2020-12-07 20:58:33 -0600

    major refactor to human-readable behaviors

--- docstring_to_readme/traverse.py
+++ docstring_to_readme/traverse.py
@@ -3,57 +3,79 @@ from docstring_to_readme import graph as g
 
 def replace_at_root(graph1: dict, graph2: dict) -> dict:
     if graph1 == graph2:
-        return graph1
+        return {}
 
-    res = _replace_at_root(graph1, graph2)
-    if res == graph1:
+    if not contains(graph1, graph2):
         children = g.node_children(graph1)
         children.append(graph2)
 
         return g.node(
             section=g.node_section(graph1),
-            pretty_section=g.node_p_section(graph1),
             body=g.node_body(graph1),
             children=children,
         )
 
-    return res
+    if not g.truth_value(graph2):
+        return remove_node(graph1, graph2)
+
+    return update_node(graph1, graph2)
 
 
-def _replace_at_root(graph1: dict, graph2: dict) -> dict:
-    if graph1 == graph2:
-        return graph1
-
-    # if pretty_section's are the same, replace with right
-    if g.node_p_section(graph1) == g.node_p_section(
-        graph2
+def contains(search_graph: dict, find_graph: dict) -> bool:
+    if g.node_p_section(search_graph) == g.node_p_section(
+        find_graph
     ):
-        return graph2
+        return True
 
-    # graph2 less # than graph1, instant falsy return
-    if g.lt(graph2, graph1):
+    children = g.node_children(search_graph)
+    for child in children:
+        return contains(child, find_graph)
+
+    return False
+
+
+def remove_node(
+    remove_from: dict, target_node: dict
+) -> dict:
+    if g.node_p_section(remove_from) == g.node_p_section(
+        target_node
+    ):
         return {}
 
-    # already know pretty sections don't match, and so if
-    # these two graphs are on the same level, we can safely
-    # assume that graph2 needs to be added
-    if g.eq(graph1, graph2):
-        return {}
+    children = g.node_children(remove_from)
+    new_children = []
 
-    children = []
-    for child in g.node_children(graph1):
-        res = _replace_at_root(child, graph2)
-        if res and g.truth_value(res):
-            children.append(res)
-        elif res and not g.truth_value(res):
-            # "remove" node
-            pass
-        else:
-            children.append(child)
+    for child in children:
+        res = remove_node(child, target_node)
+        if res:
+            new_children.append(res)
 
-    return g.node(
-        section=g.node_section(graph1),
-        pretty_section=g.node_p_section(graph1),
-        body=g.node_body(graph1),
-        children=children,
-    )
+        return g.node(
+            section=g.node_section(remove_from),
+            body=g.node_body(remove_from),
+            children=new_children,
+        )
+
+    return remove_from
+
+
+def update_node(
+    update_in: dict, target_node: dict
+) -> dict:
+    if g.node_p_section(update_in) == g.node_p_section(
+        target_node
+    ):
+        return target_node
+
+    children = g.node_children(update_in)
+    new_children = []
+
+    for child in children:
+        res = update_node(child, target_node)
+        new_children.append(res)
+
+        return g.node(
+            section=g.node_section(update_in),
+            body=g.node_body(update_in),
+            children=new_children,
+        )
--- tests/test_traverse.py
+++ tests/test_traverse.py
@@ -4,6 +4,8 @@ from json import dumps
 
 
 class test_replace_at_root(unittest.TestCase):
+
+    # @unittest.skip("")
     def test_same_graph(self):
         graph1 = {
             "section": "# Title",
@@ -17,55 +19,12 @@ class test_replace_at_root(unittest.TestCase):
             "body": "Some text",
             "children": [],
         }
-        gold = {
-            "section": "# Title",
-            "pretty_section": "# Title",
-            "body": "Some text",
-            "children": [],
-        }
-        output = replace_at_root(graph1, graph2)
-        self.assertEqual(gold, output)
-
-    def test_right_replaces_left(self):
-        graph1 = {
-            "section": "# Title",
-            "pretty_section": "# Title",
-            "body": "Some text",
-            "children": [],
-        }
-        graph2 = {
-            "section": "# Title",
-            "pretty_section": "# Title",
-            "body": "Different text",
-            "children": [],
-        }
-        gold = {
-            "section": "# Title",
-            "pretty_section": "# Title",
-            "body": "Different text",
-            "children": [],
-        }
-        output = replace_at_root(graph1, graph2)
-        self.assertEqual(gold, output)
-
-    def test_right_is_less_nested_than_left(self):
-        graph1 = {
-            "section": "## Subtitle",
-            "pretty_section": "## Subtitle",
-            "body": "some text",
-            "children": [],
-        }
-        graph2 = {
-            "section": "# Title",
-            "pretty_section": "# Title",
-            "body": "some text",
-            "children": [],
-        }
         gold = {}
         output = replace_at_root(graph1, graph2)
         self.assertEqual(gold, output)
 
-    def test_child_node_inserted(self):
+    # @unittest.skip("")
+    def test_append_new_node(self):
         graph1 = {
             "section": "# Title",
             "pretty_section": "# Title",
@@ -107,192 +66,18 @@ class test_replace_at_root(unittest.TestCase):
         output = replace_at_root(graph1, graph2)
         self.assertEqual(gold, output)
 
-    def test_child_node_replaced(self):
+    # @unittest.skip("")
+    def test_remove_child_node(self):
         graph1 = {
             "section": "# Title",
             "pretty_section": "# Title",
-            "body": "text",
+            "body": "",
             "children": [
                 {
                     "section": "## Subtitle",
                     "pretty_section": "## Subtitle",
-                    "body": "text",
-                    "children": [
-                        {
-                            "section": "### Subtitle",
-                            "pretty_section": "### Subtitle",
-                            "body": "old",
-                            "children": [],
-                        }
-                    ],
-                }
-            ],
-        }
-        graph2 = {
-            "section": "### Subtitle",
-            "pretty_section": "### Subtitle",
-            "body": "NEW",
-            "children": [],
-        }
-        gold = {
-            "section": "# Title",
-            "pretty_section": "# Title",
-            "body": "text",
-            "children": [
-                {
-                    "section": "## Subtitle",
-                    "pretty_section": "## Subtitle",
-                    "body": "text",
-                    "children": [
-                        {
-                            "section": "### Subtitle",
-                            "pretty_section": "### Subtitle",
-                            "body": "NEW",
-                            "children": [],
-                        }
-                    ],
-                }
-            ],
-        }
-        output = replace_at_root(graph1, graph2)
-        self.assertEqual(gold, output)
-
-    def test_child_node_added_at_end_deep_nesting(self):
-        """
-        If we encounter a brand new node, the desired
-        behavior is to append to the end of the root
-        graph's children
-        """
-
-        graph1 = {
-            "section": "# Title",
-            "pretty_section": "# Title",
-            "body": "text",
-            "children": [
-                {
-                    "section": "## Subtitle",
-                    "pretty_section": "## Subtitle",
-                    "body": "text",
-                    "children": [
-                        {
-                            "section": "### Subtitle",
-                            "pretty_section": "### Subtitle",
-                            "body": "text",
-                            "children": [],
-                        }
-                    ],
-                }
-            ],
-        }
-        graph2 = {
-            "section": "### Subtitle 2",
-            "pretty_section": "### Subtitle 2",
-            "body": "text",
-            "children": [],
-        }
-        gold = {
-            "section": "# Title",
-            "pretty_section": "# Title",
-            "body": "text",
-            "children": [
-                {
-                    "section": "## Subtitle",
-                    "pretty_section": "## Subtitle",
-                    "body": "text",
-                    "children": [
-                        {
-                            "section": "### Subtitle",
-                            "pretty_section": "### Subtitle",
-                            "body": "text",
-                            "children": [],
-                        },
-                    ],
-                },
-                {
-                    "section": "### Subtitle 2",
-                    "pretty_section": "### Subtitle 2",
                     "body": "text",
                     "children": [],
-                },
-            ],
-        }
-        output = replace_at_root(graph1, graph2)
-        self.assertEqual(gold, output)
-
-    def test_child_node_added_at_end_deep_middle(self):
-        graph1 = {
-            "section": "# Title",
-            "pretty_section": "# Title",
-            "body": "text",
-            "children": [
-                {
-                    "section": "## Subtitle",
-                    "pretty_section": "## Subtitle",
-                    "body": "text",
-                    "children": [
-                        {
-                            "section": "### Subtitle",
-                            "pretty_section": "### Subtitle",
-                            "body": "text",
-                            "children": [],
-                        }
-                    ],
-                }
-            ],
-        }
-        graph2 = {
-            "section": "## Subtitle 2",
-            "pretty_section": "## Subtitle 2",
-            "body": "text",
-            "children": [],
-        }
-        gold = {
-            "section": "# Title",
-            "pretty_section": "# Title",
-            "body": "text",
-            "children": [
-                {
-                    "section": "## Subtitle",
-                    "pretty_section": "## Subtitle",
-                    "body": "text",
-                    "children": [
-                        {
-                            "section": "### Subtitle",
-                            "pretty_section": "### Subtitle",
-                            "body": "text",
-                            "children": [],
-                        }
-                    ],
-                },
-                {
-                    "section": "## Subtitle 2",
-                    "pretty_section": "## Subtitle 2",
-                    "body": "text",
-                    "children": [],
-                },
-            ],
-        }
-        output = replace_at_root(graph1, graph2)
-        self.assertEqual(gold, output)
-
-    def test_falsy_child_node_removed(self):
-        graph1 = {
-            "section": "# Title",
-            "pretty_section": "# Title",
-            "body": "text",
-            "children": [
-                {
-                    "section": "## Subtitle",
-                    "pretty_section": "## Subtitle",
-                    "body": "text",
-                    "children": [
-                        {
-                            "section": "### Subtitle",
-                            "pretty_section": "### Subtitle",
-                            "body": "text",
-                            "children": [],
-                        }
-                    ],
                 }
             ],
         }
@@ -305,8 +90,45 @@ class test_replace_at_root(unittest.TestCase):
         gold = {
             "section": "# Title",
             "pretty_section": "# Title",
-            "body": "text",
+            "body": "",
             "children": [],
+        }
+        output = replace_at_root(graph1, graph2)
+        self.assertEqual(gold, output)
+
+    # @unittest.skip("")
+    def test_update_child_node(self):
+        graph1 = {
+            "section": "# Title",
+            "pretty_section": "# Title",
+            "body": "",
+            "children": [
+                {
+                    "section": "## Subtitle",
+                    "pretty_section": "## Subtitle",
+                    "body": "text",
+                    "children": [],
+                }
+            ],
+        }
+        graph2 = {
+            "section": "## Subtitle",
+            "pretty_section": "## Subtitle",
+            "body": "NEW TEXT",
+            "children": [],
+        }
+        gold = {
+            "section": "# Title",
+            "pretty_section": "# Title",
+            "body": "",
+            "children": [
+                {
+                    "section": "## Subtitle",
+                    "pretty_section": "## Subtitle",
+                    "body": "NEW TEXT",
+                    "children": [],
+                }
+            ],
         }
         output = replace_at_root(graph1, graph2)
         self.assertEqual(gold, output)
