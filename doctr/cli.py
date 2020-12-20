b6b918a05e58f1bc95d01e9af93aa80c4abea11b
kremrik <kyle.robert.e@gmail.com>
2020-12-20 11:46:50 -0600

    updated names/readme

--- doctr/cli.py
+++ doctr/cli.py
@@ -1,54 +1,5 @@
-from doctr.parsers.python_to_graph import (
-    python_to_graph,
-)
-from doctr.parsers.readme_to_graph import (
-    readme_to_graph,
-)
-from doctr.parsers.graph_to_readme import (
-    dumps,
-)
-from doctr.traverse import update
-
 import argparse
-from sys import argv
 from typing import List
-
-
-def main(arguments: List[str]) -> None:
-    args = cli(arguments)
-    path = args.path
-    path = path if not path.endswith("/") else path[:-1]
-    level = args.level
-    readme = args.readme
-
-    updated_graph = update_readme_graph(
-        readme_path=readme, doc_path=path, level=level
-    )
-
-    update_readme(graph=updated_graph, path=readme)
-
-
-def update_readme_graph(
-    readme_path: str, doc_path: str, level: int
-) -> dict:
-    try:
-        readme = open(readme_path, "r").read()
-    except FileNotFoundError:
-        readme = ""
-
-    readme_graph = readme_to_graph(readme)
-
-    doc_graph = python_to_graph(doc_path, level)
-    updated = update(readme_graph, doc_graph)
-
-    return updated
-
-
-def update_readme(graph: dict, path: str) -> None:
-    to_markdown = dumps(graph)
-
-    with open(path, "w") as readme:
-        readme.write(to_markdown)
 
 
 def cli(arguments: List[str]) -> argparse.Namespace:
@@ -81,7 +32,3 @@ def cli(arguments: List[str]) -> argparse.Namespace:
     )
 
     return parser.parse_args(arguments)
-
-
-if __name__ == "__main__":
-    main(argv[1:])
